21e8f2bc0b6f8bce14d9d717a1cdca55ec2f1fe3
masterxyth <rizwanq@sfu.ca>
2017-06-07 21:52:18 -0700

    w/db helper

--- waitercaller.py
+++ waitercaller.py
@@ -18,9 +18,14 @@ from flask_login import logout_user
 import config
 
 from bitlyhelper import BitlyHelper
-from mockdbhelper import MockDBHelper as DBHelper
+
 from passwordhelper import PasswordHelper
 from user import User
+
+if config.test
+    from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 
 DB = DBHelper()
 PH = PasswordHelper()
@@ -103,7 +108,7 @@ def account_createtable():
     form = CreateTableForm(request.form)
     if form.validate():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
-        new_url = BH.shorten_url(config.base_url+"newrequest/"+tableid)
+        new_url = BH.shorten_url(config.base_url + "newrequest/" + str(tableid))
         DB.update_table(tableid, new_url)
         return redirect(url_for('account'))
     return render_template("account.html", createtableform=form, tables = DB.get_tables(current_user.get_id()))
